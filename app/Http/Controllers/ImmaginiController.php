53cc6aa5a494c0759683da70372ae3eba9c5633b
bobbylinux <roberto.bani@gmail.com>
2015-05-13 17:49:59 +0200

    iniziata genstione server salvataggio immagini croppate

---
+++ app/Http/Controllers/ImmaginiController.php
@@ -0,0 +1,140 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Input as Input;
+use App\Http\Controllers\Controller;
+use App\Models\Immagine as Immagine;
+use Illuminate\Support\Facades\Response as Response;
+use Illuminate\Http\Request;
+
+class ImmaginiController extends Controller
+{
+
+    private $immagine;
+
+    public function __construct(Immagine $immagine) {
+        $this->immagine = $immagine;
+    }
+
+    public function getImageInstance(Immagine $immagine) {
+        return new $immagine;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        /* validazione immagini */
+        $cartella_random = str_random(15);
+        $url_file = 'uploads/' . $cartella_random;
+        $idx_imgs = array(); //indici delle immagini appena inserite da collegare al prodotto una volta salvato quest'ultimo
+        if (Input::hasFile('files')) {
+            foreach (Input::file('files') as $file) {
+                $nome_file = $file->getClientOriginalName();
+                $tipo_file = $file->guessClientExtension();
+                $data_img = array(
+                    'nome' => $nome_file,
+                    'url' => $url_file,
+                    'tipo' => $tipo_file,
+                    'file' => $file);
+                if (!$this->immagine->validate($data_img)) {
+                    $errors = $this->immagine->getErrors();
+                    return Response::json(array(
+                        'code' => '500', //KO
+                        'msg' => 'KO',
+                        'errors' => $errors));
+                }
+            }
+
+            foreach (Input::file('files') as $file) {
+                $nome_file = $file->getClientOriginalName();
+                $tipo_file = $file->guessClientExtension();
+                $data_img = array(
+                    'nome' => $nome_file,
+                    'url' => $url_file,
+                    'tipo' => $tipo_file);
+
+                $immagine = $this->getImageInstance($this->immagine); //questo è un utilizzo casareccio della Dependency Injection, almeno come io l'ho concepita....
+                $immagine->store($data_img, $file);
+                array_push($idx_imgs,$immagine->id);
+                //$this->prodotto->immagini()->attach($id);
+            }
+            return Response::json(array(
+                'code' => '200', //OK
+                'msg' => 'OK',
+                'img' => Response::json($idx_imgs)));
+        } else {
+            return Response::json(array(
+                'code' => '500', //KO
+                'msg' => 'no file in input'));
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
